7371914849e8e8d8d39ffd22faa2c003c404e673
aevartg <aevartg@gmail.com>
2016-12-09 16:53:04 +0000

    merge conflict

--- server/socket-app/tictactoe/tictactoe-state.js
+++ server/socket-app/tictactoe/tictactoe-state.js
@@ -5,15 +5,9 @@ module.exports = function (injected) {
     return function (history) {
 
         function processEvent(event) {
-<<<<<<< HEAD
-<<<<<<< HEAD
             if(event.type==="GameJoined"){
                 gamefull = true;
             }
-=======
->>>>>>> parent of 3213f86... npm test run beautifully
-=======
->>>>>>> parent of 3213f86... npm test run beautifully
         }
 
         function processEvents(history) {
