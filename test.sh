348e37a137151c7364baae137627fdf390e3acc2
aevartg <aevartg@gmail.com>
2016-12-12 14:15:40 +0000

    deploy script added

--- test.sh
+++ test.sh
@@ -78,7 +78,4 @@ fi
 cd ..
 cd ..
 
-scp -o StrictHostKeyChecking=no -i "~/ec2-lykill.pem" /var/lib/jenkins/workspace/Commit\ stage/docker-compose.yml /var/lib/jenkins/workspace/Commit\ stage/build/.env ec2-user@ec2-52-50-152-101.eu-west-1.compute.amazonaws.com:.
-ssh -o StrictHostKeyChecking=no -i "~/ec2-lykill.pem" ec2-user@ec2-52-50-152-101.eu-west-1.compute.amazonaws.com "docker-compose up"
-
 echo "Done"
